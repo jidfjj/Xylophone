80a952a686eb33157bcf51579db348c0a72a8bea
StudentJ <jidf@live.cn>
2021-04-11 13:43:45 +0900

    1st

---
+++ Xylophone/VIewController.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+class ViewController: UIViewController{
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+
+    @IBAction func notePressed(_ sender: UIButton) {
+        
+        //Test github connect
+        //t2
+        
+        
+    }
+    
+  
+
+}
+
